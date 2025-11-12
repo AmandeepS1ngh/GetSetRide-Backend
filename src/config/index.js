e607369836a1048085159eb5d44f18d42927c926
Aman <amandeep3771.beai23@chitkara.edu.in>
2025-11-12 09:35:22 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,5 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpire: process.env.JWT_EXPIRE || '7d',
   nodeEnv: process.env.NODE_ENV || 'development',
-  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173'
+  frontendUrl: process.env.FRONTEND_URL || 'https://get-set-ride-react.vercel.app'
 };
